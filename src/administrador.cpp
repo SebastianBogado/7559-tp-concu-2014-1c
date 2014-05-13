9e8f0ef3e149dc440a0a1d9e0241bba764fc5950
jmartty <jmartty@hotmail.com>
2014-05-12 23:48:11 -0300

    admin stuff

--- src/administrador.cpp
+++ src/administrador.cpp
@@ -23,28 +23,32 @@ int main(int argc, char* argv[]) {
 	Logger::notice(logMsg.str(),procName);
 
 	// TODO: Manejo de señales para que termine el proceso!
-
-	CajaRegistradora caja;
-
-	logMsg.str("");logMsg.clear();
-	logMsg << "Caja Registradora creada y lista para ser consultada";
-	Logger::debug(logMsg.str(), procName);
-
-	int sleepTime = 0;
-	double monto = 0;
-	for(;;) {
-		sleepTime = rand() % 10;
-
+	try {
+		CajaRegistradora caja;
+		caja.crearCaja();
+		
 		logMsg.str("");logMsg.clear();
-		logMsg << "El administrador esperará " << sleepTime << " segundos para consultar la caja";
+		logMsg << "Caja Registradora creada y lista para ser consultada";
 		Logger::debug(logMsg.str(), procName);
 
-		sleep(sleepTime);
-		monto = caja.consultarMonto();
+		int sleepTime = 0;
+		double monto = 0;
+		for(;;) {
+			sleepTime = rand() % 10;
 
-		logMsg.str("");logMsg.clear();
-		logMsg << "En la caja hay actualmente " << monto << " pesos";
-		Logger::notice(logMsg.str(), procName);
+			logMsg.str("");logMsg.clear();
+			logMsg << "El administrador esperará " << sleepTime << " segundos para consultar la caja";
+			Logger::debug(logMsg.str(), procName);
+
+			sleep(sleepTime);
+			monto = caja.consultarMonto();
+
+			logMsg.str("");logMsg.clear();
+			logMsg << "En la caja hay actualmente " << monto << " pesos";
+			Logger::notice(logMsg.str(), procName);
+		}
+	}catch(std::string& err) {
+		Logger::error("Excepcion: " + err, procName);
 	}
 
 	// TODO: La caja se debe destruir en el proceso padre (el que la crea!)
